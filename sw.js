b73313f77d0c5d47c6a96f1bd6b9b510a2e1c2bf
baileyburns909-debug <baileyburns909@gmail.com>
2026-09-10 23:40:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE="last-light-pro-v1";
+const CORE=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET") return;
+ e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
